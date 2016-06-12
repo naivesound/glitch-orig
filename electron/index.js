cbad28eebd05d8292c8182249998a67f732ec0a8
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2016-06-12 15:33:15 +0800

    added sample loader for electron

--- electron/index.js
+++ electron/index.js
@@ -11,12 +11,57 @@ let mainWindow;
 
 function injectPlugins() {
   mainWindow.webContents.executeJavaScript('window.userFuncs = {};');
-  var plugins = fs.readdirSync('plugins').filter(function(name) {
-    return name.endsWith('.js');
-  }).forEach(function(plugin) {
-    var code = fs.readFileSync(path.join('plugins', plugin), 'utf8');
-    mainWindow.webContents.executeJavaScript(`;(function(){${code}})();`);
-  });
+  try {
+    var plugins = fs.readdirSync('plugins').filter(function(name) {
+      return name.endsWith('.js');
+    }).forEach(function(plugin) {
+      var code = fs.readFileSync(path.join('plugins', plugin), 'utf8');
+      mainWindow.webContents.executeJavaScript(`;(function(){${code}})();`);
+    });
+  } catch (e) {}
+}
+
+function injectSamples() {
+  mainWindow.webContents.executeJavaScript(loadSamples.toString());
+  try {
+    fs.readdirSync('samples').forEach(function(sample) {
+      var wavPath = path.join('samples', sample);
+      if (sample.endsWith('.wav')) {
+        var name = JSON.stringify(sample.replace(/\.wav$/, ''));
+        mainWindow.webContents.executeJavaScript(`loadSamples(${name}, [${JSON.stringify(wavPath)}]);`);
+      } else {
+        var name = JSON.stringify(sample);
+        var variants = fs.readdirSync(wavPath).map(function(f) {
+          return path.join(wavPath, f);
+        });
+        mainWindow.webContents.executeJavaScript(`loadSamples(${name}, ${JSON.stringify(variants)});`);
+      }
+    });
+  } catch (e) {}
+}
+
+function loadSamples(name, paths) {
+  var fs = require('electron').remote.require('fs');
+  window.userFuncs[name] = function(args) {
+    this.cache = this.cache || {};
+    var index = (args[0] ? args[0]() : 0);
+    var volume = (args[1] ? args[1]() : 1);
+    let len = paths.length;
+    if (!isNaN(index) && !isNaN(volume)) {
+      let sample = paths[(((index|0)%len)+len)%len];
+      this.cache[sample] = this.cache[sample] || fs.readFileSync(sample);
+      if (this.i * 2 + 0x80 + 1 < this.cache[sample].length) {
+        let v = this.cache[sample].readInt16LE(0x80 + this.i * 2);
+        let x =  v / 0x7fff;
+        this.i++;
+        return x * volume * 127 + 128;
+      } else {
+        return NaN
+      }
+    }
+    this.i = 0;
+    return NaN
+  };
 }
 
 function createWindow () {
@@ -29,6 +74,7 @@ function createWindow () {
   });
 
   injectPlugins();
+  injectSamples();
   mainWindow.loadURL('file://' + __dirname + '/build/index.html');
   mainWindow.on('closed', function() {
     mainWindow = null;
